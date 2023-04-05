c2faffc6ef11879824f0b5552d27d457b0c79306
Ebla Ekerdiker <eblaekerdiker@gmail.com>
2023-04-05 22:57:19 +0300

    It is not done, have to work on the last 4 tests

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your solution here!
+// Write your solution here! 
+let cats = ["Milo", "Otis","Garfield"];
+ function destructivelyAppendCat(name){ cats.push('Ralph'); } 
+ function destructivelyPrependCat(name){ cats.unshift('Bob'); } 
+ function destructivelyRemoveLastCat(name){ cats.pop(); } 
+function destructivelyRemoveFirstCat(name){ cats.shift(); } 
+function appendCat(name){ cats[3] = 'Broom'; cats; } 
+function removeLastCat(){ cats.splice(0,1); }
